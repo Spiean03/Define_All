a16ecc24c3d474b961f74011a5730c85e0998058
Andreas Spielhofer <andreas.spielhofer@mail.mcgill.ca>
2017-10-06 15:27:26 -0400

    Create _defineall.py

---
+++ _defineall.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 14 18:18:26 2017
+
+@author:    Andreas Spielhofer
+            Ph.D. Candidate
+            Physics Departement
+            McGill University
+            Montreal, Canada
+@contact:   andreas.spielhofer@mail.mcgill.ca
+
+To import the file:
+import _defineall as _d
+
+To use the details in your code, e.g. Kleindiek Serial:
+_d.KLEINDIEK_SERIAL
+"""
+
+# Serial and Baudrate for the Kleindiek MM3A Micromanipulator
+KLEINDIEK_SERIAL = 'COM20'
+KLEINDIEK_BAUDRATE = 115200
+
+#IP address, Port number for the Capres Four Point-Probe System
+CAPRES_HOST = "192.168.1.80"
+CAPRES_PORT = 23
